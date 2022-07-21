8a76662fca7d9207667fa811d0485b05071d7c8a
cute_python <missudemy@gmail.com>
2022-07-21 09:55:02 +0200

    debug_heroku

--- main.py
+++ main.py
@@ -25,6 +25,6 @@ def get_name(name: str):
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-#uvicorn main:app --reload
+uvicorn main:app --reload
 
 
